470e7fad7a62f0df9629e73a613f191959cd4b8a
yoinergomez <yoiner.gomez22@gmail.com>
2017-08-27 17:00:06 -0500

    Agregando funcion de suma para los elementos de una lista

---
+++ src/main/java/co/edu/udea/pruebas_ps2/ldl/AritmeticaLDL.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.udea.pruebas_ps2.ldl;
+
+import co.edu.udea.pruebas_ps2.modelo.Tupla;
+
+/**
+ * 
+ * @author Yoiner Gómez - yoiner.gomez22@gmail.com
+ * @date 2017/08/27
+ * @version v1.0
+ */
+public class AritmeticaLDL {
+    
+    public Double sumatoriaXY(LDL lista) {
+        Double suma = new Double("0");
+        
+        return suma;
+    }
+    
+    /**
+     * Suma los datos X y Y de la lista ligada de manera independiente
+     * @author Yoiner Gómez - yoiner.gomez22@gmail.com
+     * @param lista
+     * @return 
+     */
+    public Tupla suma(LDL lista) {
+        Tupla resultado;
+        Double sumaX = new Double("0");
+        Double sumaY = new Double("0");
+        
+        NodoDoble aux = lista.getPrimerNodo();
+        while (aux != null) {
+            sumaX += getX(aux);
+            sumaY += getY(aux);
+            aux = aux.getSiguiente(); // Mover al siguiente nodo
+        }
+        
+        resultado = new Tupla(sumaX, sumaY);
+        resultado.redondearValores();
+        
+        return resultado;
+    }
+    
+    public LDL potencia(LDL lista, int exponente) {
+        return lista;
+    }
+    
+    private Double getX(NodoDoble nodo) {
+        if (nodo.getDato() != null && nodo.getDato().getX() != null) {
+            return nodo.getDato().getX();
+        }
+        return new Double(0);
+    }
+    
+    private Double getY(NodoDoble nodo) {
+        if (nodo.getDato() != null && nodo.getDato().getY() != null) {
+            return nodo.getDato().getY();
+        }
+        return new Double(0);
+    }
+}
---
+++ src/test/java/co/edu/udea/pruebas_ps2/ldl/AritmeticaLDLTest.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.udea.pruebas_ps2.ldl;
+
+import co.edu.udea.pruebas_ps2.modelo.Tupla;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ *
+ * @author Yoiner Gómez - yoiner.gomez22@gmail.com
+ */
+public class AritmeticaLDLTest {
+    
+    private LDL lista;
+    private AritmeticaLDL instancia;
+    
+    public AritmeticaLDLTest() {
+    }
+    
+    @Before
+    public void setUp() {
+        instancia = new AritmeticaLDL();
+        lista = new LDL();
+        
+        lista.insertar(new NodoDoble(new Tupla(new Double("20"), new Double("40"))));
+        lista.insertar(new NodoDoble(new Tupla(new Double("10.56"), new Double("21.12"))));
+        lista.insertar(new NodoDoble(new Tupla(new Double("19.44"), new Double("38.88"))));
+    }
+
+    /**
+     * Test of sumatoriaXY method, of class AritmeticaLDL.
+     */
+//    @Test
+//    public void testSumatoriaXY() {
+//        System.out.println("sumatoriaXY");
+//        LDL lista = null;
+//        AritmeticaLDL instance = new AritmeticaLDL();
+//        Double expResult = null;
+//        Double result = instance.sumatoriaXY(lista);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+
+    /**
+     * Prueba que la X de la lista ligada sea sumado correctamente
+     */
+    @Test
+    public void testSumaX() {
+        Tupla aux = new Tupla(new Double("50"), new Double("100"));
+        Tupla resultado = instancia.suma(lista);
+        
+        assertEquals(aux.getX(), resultado.getX());
+    }
+    
+    /**
+     * Prueba que la Y de la lista ligada sea sumada correctamente
+     */
+    @Test
+    public void testSumaY() {
+        Tupla aux = new Tupla(new Double("50"), new Double("100"));
+        Tupla resultado = instancia.suma(lista);
+        
+        assertEquals(aux.getY(), resultado.getY());
+    }
+    
+    /**
+     * Prueba que el resultado de la lista sea correcta existiendo un nodo nulo
+     */
+    @Test
+    public void testSumaNodoDobleNULL() {
+        lista.insertar(new NodoDoble());
+        
+        Tupla aux = new Tupla(new Double("50"), new Double("100"));
+        Tupla resultado = instancia.suma(lista);
+        
+        assertEquals(aux.getY(), resultado.getY());
+    }
+    
+    /**
+     * Prueba que el resultado de la lista sea correcta existiendo un nodo 
+     * con una tupla nula
+     */
+    @Test
+    public void testSumaNodoDobleConTuplaNULL() {
+        lista.insertar(new NodoDoble(new Tupla()));
+        
+        Tupla aux = new Tupla(new Double("50"), new Double("100"));
+        Tupla resultado = instancia.suma(lista);
+        
+        assertEquals(aux.getY(), resultado.getY());
+    }
+    
+    /**
+     * Prueba que el resultado de la lista sea correcta existiendo datos
+     * pertenecientes al conjunto de los reales
+     */
+    @Test
+    public void testSumaConDecimales() {
+        lista.insertar(new NodoDoble(new Tupla(new Double("0.001"), new Double(0))));
+        
+        Tupla aux = new Tupla(new Double("50.001"), new Double("100"));
+        Tupla resultado = instancia.suma(lista);
+        
+        assertEquals(aux.getX(), resultado.getX());
+    }
+    
+    /**
+     * Test of potencia method, of class AritmeticaLDL.
+     */
+//    @Test
+//    public void testPotencia() {
+//        System.out.println("potencia");
+//        LDL lista = null;
+//        int exponente = 0;
+//        AritmeticaLDL instance = new AritmeticaLDL();
+//        LDL expResult = null;
+//        LDL result = instance.potencia(lista, exponente);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+    
+}
