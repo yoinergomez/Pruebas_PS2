23e244d12a78a8d4f893dc4cd11da948ff5b4aca
yoinergomez <yoiner.gomez22@gmail.com>
2017-08-27 11:55:03 -0500

    Creación de esquema del proyecto

---
+++ src/main/java/co/edu/udea/pruebas_ps2/Principal.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.udea.pruebas_ps2;
+
+/**
+ * 
+ * @author Yoiner Gómez - yoiner.gomez22@gmail.com
+ * @date 2017/08/27
+ * @version v1.0
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
---
+++ src/test/java/co/edu/udea/pruebas_ps2/PrincipalTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.udea.pruebas_ps2;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Yoiner Gómez - yoiner.gomez22@gmail.com
+ */
+public class PrincipalTest {
+    
+    public PrincipalTest() {
+    }
+
+    /**
+     * Test of main method, of class Principal.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Principal.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+    }
+    
+}
