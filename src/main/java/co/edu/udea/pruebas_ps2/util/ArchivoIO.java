7619287692c2392741facbef9b84f8913f2f72f2
jhonatorozco <jhonatorozco@gmail.com>
2017-08-29 23:44:04 -0500

    Añadiendo casos de prueba para leer los datos de la regresión desde el archivo Excel

--- src/main/java/co/edu/udea/pruebas_ps2/util/ArchivoIO.java
+++ src/main/java/co/edu/udea/pruebas_ps2/util/ArchivoIO.java
@@ -5,18 +5,24 @@
  */
 package co.edu.udea.pruebas_ps2.util;
 
+import co.edu.udea.pruebas_ps2.ldl.LDL;
+import co.edu.udea.pruebas_ps2.ldl.NodoDoble;
+import co.edu.udea.pruebas_ps2.modelo.Tupla;
 import co.edu.udea.pruebas_ps2.util.excepcion.ValidacionPS2;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Iterator;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellReference;
 
 /**
  * Clase que se encarga de manejar la lectura y escritura de archivos.
@@ -27,12 +33,17 @@ import org.apache.poi.ss.usermodel.Workbook;
  */
 public class ArchivoIO {
 
+    private Sheet sheet;
+    private Workbook workbook;
+
     /**
-     * Método que retorna el archivo que coincide con el nombre pasado como parametro.
+     * Método que retorna el archivo que coincide con el nombre pasado como
+     * parametro.
+     *
      * @param nombreArchivo Ruta del archivo que se quiere encontrar
      * @return
      * @throws FileNotFoundException
-     * @throws ValidacionPS2 
+     * @throws ValidacionPS2
      */
     public File encontrarArchivo(String nombreArchivo) throws FileNotFoundException,
             ValidacionPS2 {
@@ -48,6 +59,98 @@ public class ArchivoIO {
 
         return f;
     }
+
+    /**
+     * Método que permite abrir el libro de Excel.
+     * @param f El archivo de Excel
+     * @return
+     * @throws FileNotFoundException
+     * @throws IOException 
+     */
+    public Workbook abrirLibroExcel(File f) throws FileNotFoundException,
+            IOException {
+        InputStream in = new FileInputStream(f);
+        Workbook w = new HSSFWorkbook(in);
+
+        return w;
+    }
+
+    /**
+     * Convierte los datos guardados en el archivo de Excel en un ArrayList de
+     * tipo LDL de dos posiciones. La posición cero es una LDL que contiene
+     * las tuplas (x,y) que serán usadas para la regresión. La posición uno es una
+     * LDL que tiene los x que el usuario quiere que la regresión estime.
+     * @param nombreArchivo Ruta del archivo.
+     * @return 
+     * @throws FileNotFoundException
+     * @throws ValidacionPS2
+     * @throws IOException 
+     */
+    public ArrayList<LDL> convertirExcelALDL(String nombreArchivo) throws FileNotFoundException,
+            ValidacionPS2, IOException {
+        File f = encontrarArchivo(nombreArchivo);
+        workbook = abrirLibroExcel(f);
+        sheet = workbook.getSheetAt(0);
+        LDL datosRegresion = new LDL();
+        LDL datosEstimacion = new LDL();
+        ArrayList<LDL> datos= new ArrayList();
+        Tupla t1, t2;
+        NodoDoble nodoT1, nodoT2;
+        int indiceColumna;
+        Iterator<Row> rowIterator = sheet.iterator();
+        while (rowIterator.hasNext()) {
+            Row fila = rowIterator.next();
+            Iterator<Cell> cellIterator = fila.cellIterator();
+            t1 = new Tupla();
+            t2 = new Tupla();
+            while (cellIterator.hasNext()) {
+                Cell celda = cellIterator.next();
+                indiceColumna = celda.getColumnIndex();
+                switch (indiceColumna) {
+                    case 0:
+                        if(esCeldaValida(celda)){
+                            t1.setX(celda.getNumericCellValue());
+                        }else{
+                            throw new ValidacionPS2("El par (x,y) está incompleto."
+                                    + " La x está vacia.");
+                        }
+                        
+                        break;
+                    case 1:
+                        if(esCeldaValida(celda)){
+                            t1.setY(celda.getNumericCellValue());
+                        }else{
+                            throw new ValidacionPS2("El par (x,y) está incompleto."
+                                    + " La y está vacia.");
+                        }
+                        break;
+                    case 2:
+                        if(esCeldaValida(celda)){
+                            t2.setX(celda.getNumericCellValue());
+                        }           
+                        break;
+                }
+            }
+            nodoT1 = new NodoDoble(t1);
+            nodoT2 = new NodoDoble(t2);
+            datosRegresion.insertar(nodoT1);
+            datosEstimacion.insertar(nodoT2);
+
+        }
+        
+        datos.add(datosRegresion);
+        datos.add(datosEstimacion);
+        return datos;
+    }
     
-    
+    public boolean esCeldaValida(Cell celda) throws ValidacionPS2 {
+        if (celda == null || celda.getCellType() == Cell.CELL_TYPE_BLANK) {
+            return false;
+        } else if (celda.getCellType() != Cell.CELL_TYPE_NUMERIC) {
+            CellReference cr = new CellReference(celda);
+            throw new ValidacionPS2("Error leyendo la celda "+cr.formatAsString());
+        }
+        return true;
+    }
+
 }
--- src/test/java/co/edu/udea/pruebas_ps2/util/ArchivoIOTest.java
+++ src/test/java/co/edu/udea/pruebas_ps2/util/ArchivoIOTest.java
@@ -5,12 +5,19 @@
  */
 package co.edu.udea.pruebas_ps2.util;
 
+import co.edu.udea.pruebas_ps2.ldl.LDL;
+import co.edu.udea.pruebas_ps2.ldl.NodoDoble;
+import co.edu.udea.pruebas_ps2.modelo.Tupla;
 import co.edu.udea.pruebas_ps2.util.excepcion.ValidacionPS2;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Objects;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang.SystemUtils;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -20,18 +27,17 @@ import static org.junit.Assert.*;
  * @author Pc1
  */
 public class ArchivoIOTest {
-    
+
     public ArchivoIO archivoIO;
-    
+
     public ArchivoIOTest() {
     }
-    
-    
+
     @Before
     public void inicializar() {
-        archivoIO=new ArchivoIO();
+        archivoIO = new ArchivoIO();
     }
-    
+
     public String corregirPath(String nombreRecurso) throws URISyntaxException {
         String path = this.getClass().getClassLoader().getResource(nombreRecurso)
                 .toURI().toString();
@@ -41,15 +47,49 @@ public class ArchivoIOTest {
         return path.substring(5);
     }
 
+    public boolean iguales(LDL l1, LDL l2) {
+        NodoDoble nodoL1 = l1.getPrimerNodo();
+        NodoDoble nodoL2 = l2.getPrimerNodo();
+        Tupla t1, t2;
+        while (nodoL1 != null && nodoL2 != null) {
+            t1 = nodoL1.getDato();
+            t2 = nodoL2.getDato();
+            if (!Objects.equals(t1.getX(), t2.getX())) {
+                return false;
+            }
 
+            if (!Objects.equals(t1.getY(), t2.getY())) {
+                return false;
+            }
+            nodoL1 = l1.siguienteNodo(nodoL1);
+            nodoL2 = l1.siguienteNodo(nodoL2);
+        }
+        return true;
+
+    }
+
+    /**
+     * Caso de prueba cuando la ruta hace referencia a un archivo que no existe.
+     * Se espera un error de tipo FileNotFoundException
+     *
+     * @throws ValidacionPS2
+     * @throws URISyntaxException
+     * @throws FileNotFoundException
+     */
     @Test(expected = FileNotFoundException.class)
     public void testAbrirArchivoInexistente() throws
             ValidacionPS2, URISyntaxException, FileNotFoundException {
         String path = "esteArchivoNoExiste.xls";
         archivoIO.encontrarArchivo(path);
     }
-        
-    
+
+    /**
+     * Caso de prueba cuando la ruta hace refrencia a un archivo existente.
+     *
+     * @throws FileNotFoundException
+     * @throws ValidacionPS2
+     * @throws URISyntaxException
+     */
     @Test
     public void testAbrirArchivoExistente() throws FileNotFoundException,
             ValidacionPS2, URISyntaxException {
@@ -57,8 +97,14 @@ public class ArchivoIOTest {
         File f = archivoIO.encontrarArchivo(path);
         assertTrue(f.exists());
     }
-    
-    
+
+    /**
+     * Caso de prueba con un archivo Excel de extensión xls.
+     *
+     * @throws FileNotFoundException
+     * @throws ValidacionPS2
+     * @throws URISyntaxException
+     */
     @Test
     public void testAbrirArchivosExcel() throws FileNotFoundException,
             ValidacionPS2, URISyntaxException {
@@ -69,6 +115,8 @@ public class ArchivoIOTest {
     }
 
     /**
+     * Caso de prueba cuando se selecciona un archivo con una extensión distinta
+     * a xls.Se espera un error d etipo ValidacionPS2
      *
      * @throws FileNotFoundException
      * @throws co.edu.udea.pruebas_ps2.util.excepcion.ValidacionPS2
@@ -80,5 +128,61 @@ public class ArchivoIOTest {
         String path = corregirPath("prueba.txt");
         archivoIO.encontrarArchivo(path);
     }
-    
+
+    /**
+     * Caso de prueba que consiste en obtener el libro de Excel.
+     *
+     * @throws FileNotFoundException
+     * @throws IOException
+     * @throws ValidacionPS2
+     * @throws URISyntaxException
+     */
+    @Test
+    public void testAbrirLibroExcel() throws FileNotFoundException, IOException,
+            ValidacionPS2, URISyntaxException {
+        String path = corregirPath("prueba.xls");
+        File f = archivoIO.encontrarArchivo(path);
+        Workbook w = archivoIO.abrirLibroExcel(f);
+        assertNotNull(w);
+    }
+
+    @Test
+    public void testConvertirExcelALDL() throws URISyntaxException,
+            ValidacionPS2, FileNotFoundException, IOException {
+        //Creacion de los datos que se esperan tener en el Excel
+        LDL datosRegresionReales = new LDL();
+        LDL datosEstimacionReales = new LDL();
+        Double x = new Double(3);
+        Double y = new Double(33);
+        NodoDoble l = new NodoDoble(new Tupla(x, y));
+        datosRegresionReales.insertar(l);
+
+        x = new Double(1);
+        y = new Double(45);
+        l = new NodoDoble(new Tupla(x, y));
+        datosRegresionReales.insertar(l);
+
+        x = new Double(2);
+        y = new Double(66);
+        l = new NodoDoble(new Tupla(x, y));
+        datosRegresionReales.insertar(l);
+
+        x = new Double(7);
+        l = new NodoDoble(new Tupla(x, null));
+        datosEstimacionReales.insertar(l);
+
+        x = new Double(8);
+        l = new NodoDoble(new Tupla(x, null));
+        datosEstimacionReales.insertar(l);
+
+        String path = corregirPath("datosRegresion.xls");
+        ArrayList<LDL> datos = archivoIO.convertirExcelALDL(path);
+        LDL datosRegresion = datos.get(0);
+        LDL datosEstimacion = datos.get(1);
+        boolean b1 = iguales(datosRegresionReales, datosRegresion);
+        boolean b2 = iguales(datosEstimacionReales, datosEstimacion);
+        assertTrue(b1 && b2);
+
+    }
+
 }
